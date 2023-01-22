b5c54ebae7b144b3d2b2bb902aa6656aa00e12f2
slamchillz <mendyslam@gmail.com>
2023-01-23 00:40:15 +0100

    added module doc string

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """
-Adds `get_hyper` method to `Server` class
+Adds `get_page` method to `Server` class
 """
 import csv
-from typing import Dict, List, Tuple, Union
+from typing import List, Tuple
 
 
 class Server:
@@ -46,26 +46,3 @@ class Server:
         assert page > 0 and page_size > 0
         startIndex, endIndex = self.index_range(page, page_size)
         return self.dataset()[startIndex:endIndex]
-
-    def get_hyper(self, page: int,
-                  page_size: int) -> Dict[str, Union[int, List[List], None]]:
-        """
-        Args:
-            page (int): page number
-            page_size (int): number of items per page
-        Returns:
-            A dictionary of the following:
-                * page_size, page, data, next_page, prev_page, total_pages
-        """
-        data = self.get_page(page, page_size)
-        totalRows = len(self.dataset())
-        prev_page = page - 1 if page > 1 else None
-        next_page = page + 1
-        if self.index_range(page, page_size)[1] >= totalRows:
-            next_page = None
-        total_pages = totalRows / page_size
-        if total_pages % 1 != 0:
-            total_pages += 1
-        return {'page_size': len(data), 'page': page,
-                'data': data, 'next_page': next_page,
-                'prev_page': prev_page, 'total_pages': int(total_pages)}
