a1bc0ab4337d5d05a2b7f9aa35dec994e5c7fb0b
slamchillz <mendyslam@gmail.com>
2023-03-01 15:48:27 +0100

    making redisclient available in global context

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -3,9 +3,10 @@
  */
 import { createClient } from "redis";
 
-let client = createClient();
+let client;
 
 (async function() {
+  client = createClient();
   client.on('error', err => console.log("Redis client not connected to the server: ", err));
   await client.connect();
   console.log("Redis client connected to the server");
